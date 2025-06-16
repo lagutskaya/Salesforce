f78b7fc67bad48311109d95eb4bb5d6aeb2c4a20
Harnioff <harnioff@gmail.com>
2025-06-16 22:35:50 +0300

    Fix bugs

--- src/test/java/tests/AccountTest.java
+++ src/test/java/tests/AccountTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class AccountTest extends BaseTest {
-    
+
     @Test
     public void checkCreateAccountFull() {
         loginPage.open()
